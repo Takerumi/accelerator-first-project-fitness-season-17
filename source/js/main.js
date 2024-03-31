d14a13ad92f88b02e29bd3b99ab0ca40488b7a58
Anastasia <mrgabrielripper@gmail.com>
2024-03-31 14:02:13 +0600

    Стилизует секцию juri под мобильное разрешение, подключает вендора swiper

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,28 @@
-// https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
+import Swiper from "swiper";
+import {Navigation, Pagination} from "swiper/modules";
+import 'swiper/css';
+
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    }
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
